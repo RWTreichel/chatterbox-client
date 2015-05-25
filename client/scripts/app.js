44b267fbdd1cb6537e193209d09e6b8da51b24ea
Richard Treichel <rwtreichel@gmail.com>
2015-05-25 17:27:56 -0400

    Initial commit.

---
+++ client/scripts/app.js
@@ -0,0 +1,22 @@
+// YOUR CODE HERE:
+
+var message = {
+  username: 'Cody',
+  text: 'booga booga! (testing)',
+  roomname: ''
+};
+
+$.ajax({
+  // This is the url you should use to communicate with the parse API server.
+  url: 'https://api.parse.com/1/classes/chatterbox',
+  type: 'POST',
+  data: JSON.stringify(message),
+  contentType: 'application/json',
+  success: function (data) {
+    console.log('chatterbox: Message sent');
+  },
+  error: function (data) {
+    // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    console.error('chatterbox: Failed to send message');
+  }
+});
