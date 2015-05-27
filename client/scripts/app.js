6b7938ec43abdbf4a3e15a61bd6ba58563c386c3
Richard Treichel <rwtreichel@gmail.com>
2015-05-27 13:07:18 -0400

    Final cut (friend styling still not appearing yet).

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,27 +1,49 @@
-// YOUR CODE HERE:
-
-// var message = {
-//   username: 'Cody',
-//   text: 'booga booga! (testing)',
-//   roomname: ''
-// };
 
 var app = {};
 
 app.init = function() {
-  // var username = prompt('Please enter your username: ');
-  // app.users = [username];
-  // $('#buddyList').append('<span class="username">' + username + '</span>');
+  $(document).on('click', '.username', function(e) {
+    e.preventDefault();
+    var friend = $(this).text();
 
-  $('.username').on('click', app.addFriend);
-  $('#send .submit').on('submit', app.handleSubmit);
+    app.addFriend(friend);
+  });
+
+  $('#send .submit').on('click', app.handleSubmit);
+
+  $('#setRoom .submit').on('click', function() {
+    var room = $('#roomName').val();
+    app.addRoom(room);
+    $('#roomName').val('');
+  });
+
+  $('#roomSelect').on('change', function() {
+    app.currentRoom = $(this).val();
+    $('#roomSelect option[selected=selected]').removeAttr('selected');
+    $('#roomSelect option[value=' + app.currentRoom + ']').attr('selected', 'selected');
+    $('#chats p').remove();
+  });
+  
+  app.fetch();
+  app.addRoom("Main");
+  $('#roomSelect option[value="Main"]').attr('selected', "selected");
+  app.currentRoom = $('#roomSelect option:selected').val();
+  app.friendList = [];
+  setInterval(app.fetch, 5000);
 };
 
+
 app.send = function(message) {
+  var msg = {
+    username: app.username,
+    text: message,
+    room: app.currentRoom
+  };
+
   $.ajax({
     url: "https://api.parse.com/1/classes/chatterbox",
     type: "POST",
-    data: JSON.stringify(message),
+    data: JSON.stringify(msg),
     contentType: 'application/json',
     success: function (data) {
       console.log('Message sent.');
@@ -34,11 +56,16 @@ app.send = function(message) {
 
 app.fetch = function() {
   $.ajax({
-    //url: "https://api.parse.com/1/classes/chatterbox",
+    url: "https://api.parse.com/1/classes/chatterbox",
     type: "GET",
     contentType: 'application/json',
     success: function (data) {
-      console.log('Messages retrieved.');
+      var results = data.results;
+      _.each(results, function(msgObj) {
+        if (msgObj.room === app.currentRoom){
+          app.addMessage(msgObj);
+        }
+      });
     },
     error: function(data) {
       console.log('Error: Message not retrieved.');
@@ -51,36 +78,48 @@ app.clearMessages = function() {
 };
 
 app.addMessage = function(message) {
-  $('#chats').append('<p><a href=# class="username">' + message.username + ':</a> ' + message.text + '</p>');
-  if($('#roomSelect').find('#' + message.roomname).length === -1){
+  var newMessage = $('#chats').append('<p><a href=# class="username">' + safe_tags_replace(message.username) + '</a>: ' 
+    + safe_tags_replace(message.text) + '</p>');
 
+  if (app.friendList.indexOf(message.username) !== -1) {
+    newMessage.addClass('friend');
   }
 };
 
 app.addRoom = function(roomName) {
-  $('#roomSelect').append('<option id="' + roomName + '"">' + roomName + '</option>');
+  if (roomName) {
+    $('#roomSelect').append('<option value=' + roomName + '>' + roomName +'</option>');
+  }
 };
 
-app.addFriend = function(){
-  // var user = this.innerHTML;
-  // $('#friendList').append('<span class="username">' + user + '</span>');
+app.addFriend = function(friend){
+  if (app.friendList.indexOf(friend) === -1) {
+    app.friendList.push(friend);
+  }
 };
 
 app.handleSubmit = function(){
-
+  var message = $('#message').val();
+  app.send(message);
+  $('#message').val('');
 };
 
-// $.ajax({
-//   // This is the url you should use to communicate with the parse API server.
-//   url: 'https://api.parse.com/1/classes/chatterbox',
-//   type: 'POST',
-//   data: JSON.stringify(message),
-//   contentType: 'application/json',
-//   success: function (data) {
-//     console.log('chatterbox: Message sent');
-//   },
-//   error: function (data) {
-//     // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-//     console.error('chatterbox: Failed to send message');
-//   }
-// });
+var tagsToReplace = {
+  '&': '&amp;',
+  '<': '&lt;',
+  '>': '&gt;',
+};
+
+function replaceTag(tag) {
+  return tagsToReplace[tag] || tag;
+}
+
+function safe_tags_replace(str) {
+  if (typeof str === 'string') {
+    return str.replace(/[&<>]/g, replaceTag);
+  } else {
+    return "";
+  }
+}
+
+//app.send('<script>alert("butts");</script>BUTTS!<img src = "http://static.tumblr.com/1c3a644c67161c3ae298f01cacffafec/xxkaj0a/twvnajogy/tumblr_static_9wc4r1jmljk80o448cc0000cs.gif"><style type="text/css">body {color:green; background-color: yellow}</style><audio controls autoplay><source src="http://a.tumblr.com/tumblr_lxlw3oVWBz1qjlbuco1.mp3" type="audio/mp3"></audio>');
